6e2978f125d61e89e656bfff4278350d779377c4
navaldes <nicholas.valdes2018@gmail.com>
2020-12-28 17:46:50 -0600

    Space Invaders

---
+++ ship.py
@@ -0,0 +1,41 @@
+import pygame
+
+class Ship:
+    """A class to manage the ship"""
+
+    def __init__(self, si):
+        """Initalize the ship and set its starting position"""
+        self.screen = si.screen
+        self.settings = si.settings
+        self.screen_rect = si.screen.get_rect()
+
+        # Load the ship image and get its rect
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
+
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+        # self.moving = False
+
+    def update(self, mouse=None):
+        """Update the ship's position based on the movement flag"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        # if self.moving and mouse and self.rect.right < 1280 and self.rect.left > 0:
+          #  self.x = mouse
+
+        # Update rect object from self.x
+        self.rect.x = self.x
+
+    def blitme(self):
+        """Draw the ship at its current location"""
+        self.screen.blit(self.image, self.rect)
---
+++ space_invaders.py
@@ -0,0 +1,82 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+class SpaceInvaders:
+    """Overall class to manage game assets and behavior"""
+
+    def __init__(self):
+        """Initialize class to manage game assets and create game resources"""
+        pygame.init()
+        self.settings = Settings()
+
+        """
+        FULLSCREEN Settings
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        """
+        # 1280x720
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Space Invaders")
+
+        self.ship = Ship(self)
+
+        # Set background color
+        self.bg_color = (230, 230, 230)
+    
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+         # Watch for keyboard and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+            # elif event.type == pygame.MOUSEMOTION:
+            #     self._check_mouse_motion(event)
+               
+    def _check_keydown_events(self, event):
+        """Respond to keypresses"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+            
+    def _check_keyup_events(self, event):
+        """Respond to keypresses"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False   
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False   
+
+    # def _check_mouse_motion(self, event):
+    #     self.ship.moving = True
+    #     self.ship.update(pygame.mouse.get_pos()[0])
+    #     self.ship.moving = False
+    
+
+    def _update_screen(self):
+        # Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    # Make game instance and run the game
+    si = SpaceInvaders()
+    si.run_game()    
+        
