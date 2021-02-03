0df001d8734371e5ffd135dfcb98a26f8d806b31
navaldes <Nicholas.Valdes2018@gmail.com>
2021-02-02 22:45:55 -0600

    Done

--- ship.py
+++ ship.py
@@ -1,10 +1,12 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ship:
+class Ship(Sprite):
     """A class to manage the ship"""
 
     def __init__(self, si):
         """Initalize the ship and set its starting position"""
+        super().__init__()
         self.screen = si.screen
         self.settings = si.settings
         self.screen_rect = si.screen.get_rect()
